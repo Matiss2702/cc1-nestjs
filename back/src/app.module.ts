633cb6acb43bcbfec52f0e39b42067d277b8253d
Matiss Haillouy <matiss.haillouy@gmail.com>
2025-04-12 14:38:12 +0200

    chat back websocket

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
